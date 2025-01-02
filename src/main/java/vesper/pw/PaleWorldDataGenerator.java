90e5d3c05fc1c92ea6972e244c92b1f495cec57c
Ves <vesmaybevesper@gmail.com>
2025-01-02 12:28:46 -0500

    Add files via upload

---
+++ src/main/java/vesper/pw/PaleWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package vesper.pw;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PaleWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
