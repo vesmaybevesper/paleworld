cbbe633f02051e97fdd59501e0efac5ba5643677
Ves <vesmaybevesper@gmail.com>
2025-01-14 11:46:02 -0500

    Change pale cave region file name

--- src/main/java/vesper/pw/biomes/PaleWorldTerrablenderApi.java
+++ src/main/java/vesper/pw/biomes/PaleWorldTerrablenderApi.java
@@ -1,6 +1,5 @@
 package vesper.pw.biomes;
 
-import terrablender.api.RegionType;
 import terrablender.api.Regions;
 import terrablender.api.SurfaceRuleManager;
 import terrablender.api.TerraBlenderApi;
@@ -13,7 +12,7 @@ public class PaleWorldTerrablenderApi implements TerraBlenderApi {
 
     @Override
     public void onTerraBlenderInitialized(){
-        Regions.register(new PaleWorldRegion(Identifier.of(MOD_ID, "overworld"), 4));
+        Regions.register(new PaleCaveRegion(Identifier.of(MOD_ID, "overworld"), 4));
 
 
         SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, MOD_ID, PaleWorldMaterialRules.makeRules());
