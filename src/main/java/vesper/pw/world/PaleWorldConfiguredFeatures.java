11146e273ba53416e21f0ec030d4029d4c113ff3
Ves <vesmaybevesper@gmail.com>
2025-01-14 11:42:00 -0500

    update Clay features

--- src/main/java/vesper/pw/world/PaleWorldConfiguredFeatures.java
+++ src/main/java/vesper/pw/world/PaleWorldConfiguredFeatures.java
@@ -5,7 +5,10 @@ import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.state.property.Properties;
 import net.minecraft.structure.processor.StructureProcessorList;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DataPool;
@@ -23,10 +26,10 @@ import net.minecraft.world.gen.stateprovider.RandomizedIntBlockStateProvider;
 import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
 import vesper.pw.PaleWorld;
 import vesper.pw.block.PaleWorldBlocks;
-
+import vesper.pw.block.custom.PaleVineBodyBlock;
+import net.minecraft.block.BlockState;
+import vesper.pw.block.custom.SmallDyingDripleafBlock;
 import java.util.List;
-
-import static net.minecraft.world.gen.feature.UndergroundConfiguredFeatures.DRIPLEAF;
 
 public class PaleWorldConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?,?>> PALE_CAVE_PATCH = registryKey("pale_cave_patch");
@@ -37,6 +40,31 @@ public class PaleWorldConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?,?>> CLAY_DRIPLEAF = registryKey("clay_dripleaf");
     public static final RegistryKey<ConfiguredFeature<?,?>> CLAY_POOL_DRIPLEAF = registryKey("clay_pool_dripleaf");
     public static final RegistryKey<ConfiguredFeature<?,?>> PALE_VINE = registryKey("pale_vine_feature");
+    public static final RegistryKey<ConfiguredFeature<?,?>> DYING_DRIPLEAF = registryKey("dying_dripleaves");
+
+
+    private static RegistryEntry<PlacedFeature> createSmallDyingDripleaf(){
+        return PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(new WeightedBlockStateProvider(DataPool.<BlockState>builder()
+                        .add((BlockState) PaleWorldBlocks.SMALL_DYING_DRIPLEAF.getDefaultState().with(SmallDyingDripleafBlock.FACING, Direction.EAST), 1)
+                        .add((BlockState) PaleWorldBlocks.SMALL_DYING_DRIPLEAF.getDefaultState().with(SmallDyingDripleafBlock.FACING, Direction.WEST), 1)
+                        .add((BlockState) PaleWorldBlocks.SMALL_DYING_DRIPLEAF.getDefaultState().with(SmallDyingDripleafBlock.FACING, Direction.NORTH), 1)
+                        .add((BlockState) PaleWorldBlocks.SMALL_DYING_DRIPLEAF.getDefaultState().with(SmallDyingDripleafBlock.FACING, Direction.SOUTH), 1))),
+                new PlacementModifier[0]
+        );
+    }
+
+    private static RegistryEntry<PlacedFeature> createBigDyingDripleaf(Direction direction) {
+        return PlacedFeatures.createEntry(Feature.BLOCK_COLUMN, new BlockColumnFeatureConfig(List.of(BlockColumnFeatureConfig.createLayer(new WeightedListIntProvider(DataPool.<IntProvider>builder()
+                .add(UniformIntProvider.create(0, 4), 2)
+                .add(ConstantIntProvider.create(0), 1).build()),
+                BlockStateProvider.of((BlockState)PaleWorldBlocks.BIG_DYING_DRIPLEAF_STEM.getDefaultState()
+                        .with(Properties.HORIZONTAL_FACING, direction))),
+                BlockColumnFeatureConfig.createLayer(ConstantIntProvider.create(1),
+                        BlockStateProvider.of((BlockState)PaleWorldBlocks.BIG_DYING_DRIPLEAF.getDefaultState()
+                                .with(Properties.HORIZONTAL_FACING, direction)))),
+                        Direction.UP, BlockPredicate.IS_AIR_OR_WATER, true),
+                new PlacementModifier[0]);
+    }
 
     public static void bootstrap(Registerable<ConfiguredFeature<?,?>> configuredFeatureRegisterable) {
         RegistryEntryLookup<ConfiguredFeature<?, ?>> registryEntryLookup = configuredFeatureRegisterable.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
@@ -47,14 +75,25 @@ public class PaleWorldConfiguredFeatures {
 
         RandomizedIntBlockStateProvider randomizedIntBlockStateProvider = new RandomizedIntBlockStateProvider(new WeightedBlockStateProvider(DataPool.<BlockState>builder()
                 .add(PaleWorldBlocks.PALE_VINE.getDefaultState(), 1)
-                .add((BlockState)PaleWorldBlocks.PALE_VINE.getDefaultState(), 1)), CaveVinesHeadBlock.AGE, UniformIntProvider.create(20, 23));
+                .add((BlockState)PaleWorldBlocks.PALE_VINE.getDefaultState(), 1)), PaleVineBodyBlock.AGE, UniformIntProvider.create(20, 23));
 
-
+        register(configuredFeatureRegisterable,
+                DYING_DRIPLEAF,
+                Feature.SIMPLE_RANDOM_SELECTOR,
+                new SimpleRandomFeatureConfig(
+                        RegistryEntryList.of(new RegistryEntry[]{
+                                createSmallDyingDripleaf(),
+                                createBigDyingDripleaf(Direction.EAST),
+                                createBigDyingDripleaf(Direction.WEST),
+                                createBigDyingDripleaf(Direction.SOUTH),
+                                createBigDyingDripleaf(Direction.NORTH)})
+                )
+                );
 
        register(configuredFeatureRegisterable, PALE_CAVE_VEG, Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(new WeightedBlockStateProvider(
                 DataPool.<BlockState>builder()
                         .add(Blocks.DEAD_BUSH.getDefaultState(),5)
-                        .add(Blocks.AZALEA.getDefaultState(),2)
+                        .add(PaleWorldBlocks.DYING_AZALEA.getDefaultState(),2)
                         .add(Blocks.PALE_MOSS_CARPET.getDefaultState(), 25)
                         .add(Blocks.SHORT_GRASS.getDefaultState(),30)
                         .add(Blocks.TALL_GRASS.getDefaultState(),7))));
@@ -139,7 +178,7 @@ public class PaleWorldConfiguredFeatures {
                new VegetationPatchFeatureConfig(
                        BlockTags.LUSH_GROUND_REPLACEABLE,
                        BlockStateProvider.of(Blocks.CLAY),
-                       PlacedFeatures.createEntry(registryEntryLookup.getOrThrow(DRIPLEAF)),
+                       PlacedFeatures.createEntry(registryEntryLookup.getOrThrow(DYING_DRIPLEAF)),
                        VerticalSurfaceType.FLOOR,
                        ConstantIntProvider.create(3),
                        0.8F,
@@ -156,7 +195,7 @@ public class PaleWorldConfiguredFeatures {
                new VegetationPatchFeatureConfig(
                        BlockTags.LUSH_GROUND_REPLACEABLE,
                        BlockStateProvider.of(Blocks.CLAY),
-                       PlacedFeatures.createEntry(registryEntryLookup.getOrThrow(DRIPLEAF)),
+                       PlacedFeatures.createEntry(registryEntryLookup.getOrThrow(DYING_DRIPLEAF)),
                        VerticalSurfaceType.FLOOR,
                        ConstantIntProvider.create(3),
                        0.8F,
