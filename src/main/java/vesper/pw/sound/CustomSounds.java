db3f34ff1f9b2ebb08f0840c410bd88243abcd1b
Ves <vesmaybevesper@gmail.com>
2025-01-14 21:43:41 -0500

    Initial custom sound set-up

---
+++ src/main/java/vesper/pw/sound/CustomSounds.java
@@ -0,0 +1,26 @@
+package vesper.pw.sound;
+
+
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import vesper.pw.PaleWorld;
+
+public class CustomSounds {
+    private CustomSounds(){
+
+    }
+
+    public static final SoundEvent PALE_CAVE_ATMOS = registerSounds("pale-cave-ambi");
+    public static final SoundEvent ENGINE_LOOP = registerSounds("engine");
+
+    public static SoundEvent registerSounds(String id) {
+        Identifier identifier = Identifier.of(PaleWorld.MOD_ID, id);
+        return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.of(identifier));
+    }
+
+    public static void init(){
+
+    }
+}
