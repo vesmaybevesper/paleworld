33b70804165354af319fd4389cf9963929b58ce7
Ves <vesmaybevesper@gmail.com>
2025-01-22 18:29:14 -0500

    Half working Pale Axolotl Animations

--- src/main/java/vesper/pw/entity/PaleAxolotl/PaleAxolotlModel.java
+++ src/main/java/vesper/pw/entity/PaleAxolotl/PaleAxolotlModel.java
@@ -1,9 +1,12 @@
 package vesper.pw.entity.PaleAxolotl;
 
 import net.minecraft.client.model.*;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import vesper.pw.PaleWorld;
 
 public class PaleAxolotlModel extends EntityModel<PaleAxolotlRenderState> {
@@ -15,12 +18,9 @@ public class PaleAxolotlModel extends EntityModel<PaleAxolotlRenderState> {
     private final ModelPart bone9;
     private final ModelPart bone8;
     private final ModelPart bone7;
-    private final ModelPart bone6;
-    private final ModelPart bone5;
-    private final ModelPart bone4;
-    private final ModelPart bone3;
-    private final ModelPart bone2;
-    private final ModelPart bone;
+    private final ModelPart leftGills;
+    private final ModelPart topGills;
+    private final ModelPart rightGills;
     private final ModelPart Tail;
     private final ModelPart Body;
 
@@ -31,12 +31,9 @@ public class PaleAxolotlModel extends EntityModel<PaleAxolotlRenderState> {
         this.bone9 = root.getChild("bone9");
         this.bone8 = root.getChild("bone8");
         this.bone7 = root.getChild("bone7");
-        this.bone6 = root.getChild("bone6");
-        this.bone5 = root.getChild("bone5");
-        this.bone4 = root.getChild("bone4");
-        this.bone3 = root.getChild("bone3");
-        this.bone2 = root.getChild("bone2");
-        this.bone = root.getChild("bone");
+        this.leftGills = root.getChild("leftGills");
+        this.topGills = root.getChild("topGills");
+        this.rightGills = root.getChild("rightGills");
         this.Tail = root.getChild("Tail");
         this.Body = root.getChild("Body");
     }
@@ -69,27 +66,24 @@ public class PaleAxolotlModel extends EntityModel<PaleAxolotlRenderState> {
 
         ModelPartData cube_r8 = bone7.addChild("cube_r8", ModelPartBuilder.create().uv(2, 13).cuboid(-7.0F, 0.0F, -1.0F, 1.0F, 0.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(-1.0F, 0.0F, 7.0F, 0.0F, -1.5708F, 0.0F));
 
-        ModelPartData bone6 = modelPartData.addChild("bone6", ModelPartBuilder.create().uv(12, 45).cuboid(1.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
+        ModelPartData leftGills = modelPartData.addChild("leftGills", ModelPartBuilder.create().uv(12, 41).cuboid(1.0F, -6.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
+                .uv(10, 42).cuboid(0.0F, -5.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
+                .uv(13, 40).cuboid(2.0F, -7.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
+                .uv(12, 45).cuboid(1.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
                 .uv(11, 46).cuboid(0.0F, -1.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(4.0F, 24.0F, -4.0F));
 
-        ModelPartData bone5 = modelPartData.addChild("bone5", ModelPartBuilder.create().uv(0, 44).cuboid(-3.1816F, -3.0F, -0.5277F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F))
+        ModelPartData topGills = modelPartData.addChild("topGills", ModelPartBuilder.create().uv(4, 39).cuboid(-4.0F, -1.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
+                .uv(3, 37).cuboid(-5.0F, -3.0F, 0.0F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F))
+                .uv(10, 37).cuboid(2.0F, -3.0F, 0.0F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F))
+                .uv(8, 39).cuboid(0.0F, -1.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
+                .uv(9, 38).cuboid(1.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
+                .uv(3, 38).cuboid(-5.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(1.0F, 19.0F, -4.0F));
+
+        ModelPartData rightGills = modelPartData.addChild("rightGills", ModelPartBuilder.create().uv(2, 42).cuboid(-1.0126F, -5.0F, -0.9843F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
+                .uv(1, 41).cuboid(-2.0126F, -6.0F, -0.9843F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
+                .uv(0, 44).cuboid(-3.1816F, -3.0F, -0.5277F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F))
                 .uv(1, 46).cuboid(-2.1816F, -1.0F, -0.5277F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
                 .uv(0, 45).cuboid(-3.1816F, -2.0F, -0.5277F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-4.0F, 24.0F, -4.0F, 0.0F, -0.2182F, 0.0F));
-
-        ModelPartData bone4 = modelPartData.addChild("bone4", ModelPartBuilder.create().uv(13, 40).cuboid(2.0F, -4.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
-                .uv(10, 42).cuboid(0.0F, -2.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
-                .uv(12, 41).cuboid(1.0F, -3.0F, 0.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(4.0F, 21.0F, -4.0F));
-
-        ModelPartData bone3 = modelPartData.addChild("bone3", ModelPartBuilder.create().uv(10, 37).cuboid(2.0F, -3.0F, 0.0F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F))
-                .uv(8, 39).cuboid(0.0F, -1.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
-                .uv(9, 38).cuboid(1.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(1.0F, 19.0F, -4.0F));
-
-        ModelPartData bone2 = modelPartData.addChild("bone2", ModelPartBuilder.create().uv(4, 39).cuboid(-2.0F, -1.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
-                .uv(3, 38).cuboid(-3.0F, -2.0F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
-                .uv(3, 37).cuboid(-3.0F, -3.0F, 0.0F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(-1.0F, 19.0F, -4.0F));
-
-        ModelPartData bone = modelPartData.addChild("bone", ModelPartBuilder.create().uv(2, 42).cuboid(-1.0126F, -2.0F, 0.0157F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
-                .uv(1, 41).cuboid(-2.0126F, -3.0F, 0.0157F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-4.0F, 21.0F, -5.0F, 0.0F, -0.2182F, 0.0F));
 
         ModelPartData Tail = modelPartData.addChild("Tail", ModelPartBuilder.create().uv(10, 27).cuboid(0.0F, -3.0F, 0.0F, 0.0F, 5.0F, 4.0F, new Dilation(0.0F))
                 .uv(8, 29).cuboid(0.0F, -3.0F, 4.0F, 0.0F, 5.0F, 2.0F, new Dilation(0.0F))
@@ -105,8 +99,199 @@ public class PaleAxolotlModel extends EntityModel<PaleAxolotlRenderState> {
 
     public void setAngles(PaleAxolotlRenderState paleAxolotlRenderState) {
         super.setAngles(paleAxolotlRenderState);
+        float f = paleAxolotlRenderState.playingDeadValue;
+        float g = paleAxolotlRenderState.inWaterValue;
+        float h = paleAxolotlRenderState.onGroundValue;
+        float i = paleAxolotlRenderState.isMovingValue;
+        float j = 1.0F - i;
+        float k = 1.0F - Math.min(h, i);
+        Body.yaw += paleAxolotlRenderState.yawDegrees * ((float)Math.PI / 180F);
+        this.setMovingInWaterAngles(paleAxolotlRenderState.age, paleAxolotlRenderState.pitch, Math.min(i, g));
+        this.setStandingInWaterAngles(paleAxolotlRenderState.age, Math.min(j, g));
+        this.setMovingOnGroundAngles(paleAxolotlRenderState.age, Math.min(i, h));
+        this.setStandingOnGroundAngles(paleAxolotlRenderState.age, Math.min(j, h));
+        this.setPlayingDeadAngles(f);
+        this.copyLegAngles(k);
+    }
 
+    private void setMovingInWaterAngles(float f, float headPitch, float g) {
+        if (!(g <= 1.0E-5F)) {
+            float h = f * 0.33F;
+            float i = MathHelper.sin(h);
+            float j = MathHelper.cos(h);
+            float k = 0.13F * i;
+            ModelPart var10000 = this.Body;
+            var10000.pitch += (headPitch * ((float)Math.PI / 180F) + k) * g;
+            var10000 = this.head;
+            var10000.pitch -= k * 1.8F * g;
+            var10000 = this.Body;
+            var10000.pivotY -= 0.45F * j * g;
+            var10000 = this.topGills;
+            var10000.pitch += (-0.5F * i - 0.8F) * g;
+            float l = (0.3F * i + 0.9F) * g;
+            var10000 = this.leftGills;
+            var10000.yaw += l;
+            var10000 = this.rightGills;
+            var10000.yaw -= l;
+            var10000 = this.Tail;
+            var10000.yaw += 0.3F * MathHelper.cos(h * 0.9F) * g;
+            var10000 = this.bone10;
+            var10000.pitch += 1.8849558F * g;
+            var10000 = this.bone10;
+            var10000.yaw += -0.4F * i * g;
+            var10000 = this.bone10;
+            var10000.roll += ((float)Math.PI / 2F) * g;
+            var10000 = this.bone9;
+            var10000.pitch += 1.8849558F * g;
+            var10000 = this.bone9;
+            var10000.yaw += (-0.2F * j - 0.1F) * g;
+            var10000 = this.bone9;
+            var10000.roll += ((float)Math.PI / 2F) * g;
+        }
+    }
 
+    private void setStandingInWaterAngles(float f, float g) {
+        if (!(g <= 1.0E-5F)) {
+            float h = f * 0.075F;
+            float i = MathHelper.cos(h);
+            float j = MathHelper.sin(h) * 0.15F;
+            float k = (-0.15F + 0.075F * i) * g;
+            ModelPart var10000 = this.Body;
+            var10000.pitch += k;
+            var10000 = this.Body;
+            var10000.pivotY -= j * g;
+            var10000 = this.head;
+            var10000.pitch -= k;
+            var10000 = this.topGills;
+            var10000.pitch += 0.2F * i * g;
+            float l = (-0.3F * i - 0.19F) * g;
+            var10000 = this.leftGills;
+            var10000.yaw += l;
+            var10000 = this.rightGills;
+            var10000.yaw -= l;
+            var10000 = this.bone10;
+            var10000.pitch += (2.3561945F - i * 0.11F) * g;
+            var10000 = this.bone10;
+            var10000.yaw += 0.47123894F * g;
+            var10000 = this.bone10;
+            var10000.roll += 1.7278761F * g;
+            var10000 = this.bone9;
+            var10000.pitch += (((float)Math.PI / 4F) - i * 0.2F) * g;
+            var10000 = this.bone9;
+            var10000.yaw += 2.042035F * g;
+            var10000 = this.Tail;
+            var10000.yaw += 0.5F * i * g;
+        }
+    }
+
+    private void setMovingOnGroundAngles(float animationProgress, float headYaw) {
+        if (!(headYaw <= 1.0E-5F)) {
+            float f = animationProgress * 0.11F;
+            float g = MathHelper.cos(f);
+            float h = (g * g - 2.0F * g) / 5.0F;
+            float i = 0.7F * g;
+            float j = 0.09F * g * headYaw;
+            ModelPart var10000 = this.head;
+            var10000.yaw += j;
+            var10000 = this.Tail;
+            var10000.yaw += j;
+            float k = (0.6F - 0.08F * (g * g + 2.0F * MathHelper.sin(f))) * headYaw;
+            var10000 = this.topGills;
+            var10000.pitch += k;
+            var10000 = this.leftGills;
+            var10000.yaw -= k;
+            var10000 = this.rightGills;
+            var10000.yaw += k;
+            float l = 0.9424779F * headYaw;
+            float m = 1.0995574F * headYaw;
+            var10000 = this.bone10;
+            var10000.pitch += l;
+            var10000 = this.bone10;
+            var10000.yaw += (1.5F - h) * headYaw;
+            var10000 = this.bone10;
+            var10000.roll += -0.1F * headYaw;
+            var10000 = this.bone9;
+            var10000.pitch += m;
+            var10000 = this.bone9;
+            var10000.yaw += (((float)Math.PI / 2F) - i) * headYaw;
+            var10000 = this.bone9;
+            var10000.pitch += l;
+            var10000 = this.bone8;
+            var10000.yaw += (-1.0F - h) * headYaw;
+            var10000 = this.bone7;
+            var10000.pitch += m;
+            var10000 = this.bone7;
+            var10000.yaw += ((-(float)Math.PI / 2F) - i) * headYaw;
+        }
+    }
+
+    private void setStandingOnGroundAngles(float animationProgress, float headYaw) {
+        if (!(headYaw <= 1.0E-5F)) {
+            float f = animationProgress * 0.09F;
+            float g = MathHelper.sin(f);
+            float h = MathHelper.cos(f);
+            float i = g * g - 2.0F * g;
+            float j = h * h - 3.0F * g;
+            ModelPart var10000 = this.head;
+            var10000.pitch += -0.09F * i * headYaw;
+            var10000 = this.head;
+            var10000.roll += -0.2F * headYaw;
+            var10000 = this.Tail;
+            var10000.yaw += (-0.1F + 0.1F * i) * headYaw;
+            float k = (0.6F + 0.05F * j) * headYaw;
+            var10000 = this.topGills;
+            var10000.pitch += k;
+            var10000 = this.leftGills;
+            var10000.yaw -= k;
+            var10000 = this.rightGills;
+            var10000.yaw += k;
+            var10000 = this.bone10;
+            var10000.pitch += 1.1F * headYaw;
+            var10000 = this.bone10;
+            var10000.yaw += 1.0F * headYaw;
+            var10000 = this.bone9;
+            var10000.pitch += 0.8F * headYaw;
+            var10000 = this.bone9;
+            var10000.yaw += 2.3F * headYaw;
+            var10000 = this.bone9;
+            var10000.roll -= 0.5F * headYaw;
+        }
+    }
+
+    private void setPlayingDeadAngles(float headYaw) {
+        if (!(headYaw <= 1.0E-5F)) {
+            ModelPart var10000 = this.bone10;
+            var10000.pitch += 1.4137167F * headYaw;
+            var10000 = this.bone10;
+            var10000.yaw += 1.0995574F * headYaw;
+            var10000 = this.bone10;
+            var10000.roll += ((float)Math.PI / 4F) * headYaw;
+            var10000 = this.bone9;
+            var10000.pitch += ((float)Math.PI / 4F) * headYaw;
+            var10000 = this.bone9;
+            var10000.yaw += 2.042035F * headYaw;
+            var10000 = this.Body;
+            var10000.pitch += -0.15F * headYaw;
+            var10000 = this.Body;
+            var10000.roll += 0.35F * headYaw;
+        }
+    }
+
+    private void copyLegAngles(float f) {
+        if (!(f <= 1.0E-5F)) {
+            ModelPart var10000 = this.bone8;
+            var10000.pitch += this.bone10.pitch * f;
+            ModelPart var2 = this.bone8;
+            var2.yaw += -this.bone10.yaw * f;
+            var2 = this.bone8;
+            var2.roll += -this.bone10.roll * f;
+            var10000 = this.bone7;
+            var10000.pitch += this.bone9.pitch * f;
+            var2 = this.bone7;
+            var2.yaw += -this.bone9.yaw * f;
+            var2 = this.bone7;
+            var2.roll += -this.bone9.roll * f;
+        }
     }
 
 
