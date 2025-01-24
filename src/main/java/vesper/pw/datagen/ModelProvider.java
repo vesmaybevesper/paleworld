3f136ab90b80214f46a18102cdb459fe3dbd1e65
Ves <vesmaybevesper@gmail.com>
2025-01-23 22:53:12 -0500

    Add Pale Axolotl Spawn Egg

--- src/main/java/vesper/pw/datagen/ModelProvider.java
+++ src/main/java/vesper/pw/datagen/ModelProvider.java
@@ -4,9 +4,14 @@ import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Model;
 import net.minecraft.client.data.Models;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 import vesper.pw.block.PaleWorldBlocks;
+import vesper.pw.item.PaleWorldItems;
+
+import java.util.Optional;
 
 public class ModelProvider extends FabricModelProvider {
     public ModelProvider(FabricDataOutput output) {
@@ -21,5 +26,6 @@ public class ModelProvider extends FabricModelProvider {
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
         itemModelGenerator.register(Item.fromBlock(PaleWorldBlocks.PALE_VINE), Models.CROP);
+        itemModelGenerator.register(PaleWorldItems.PALE_AXOLOTL_SPAWN_EGG, new Model(Optional.of(Identifier.of("item/template_spawn_egg")), Optional.empty()));
     }
 }
