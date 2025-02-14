f8d11de74c9b66f4f3608fa635de28621130dcde
Ves <vesmaybevesper@gmail.com>
2025-02-13 20:20:47 -0500

    Add Pale Cave weight to config

--- src/main/java/vesper/pw/PaleWorldConfig.java
+++ src/main/java/vesper/pw/PaleWorldConfig.java
@@ -3,7 +3,8 @@ package vesper.pw;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class PaleWorldConfig extends MidnightConfig {
-    
-    @Comment public static Comment wip;
+
+    @Comment public static Comment warn;
+    @Entry public static int palecaveweight = 4;
 
 }
