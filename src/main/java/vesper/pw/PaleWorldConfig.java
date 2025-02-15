f7ff2b1acfc9f5965279b382f83c6f145ce8c6f8
Ves <vesmaybevesper@gmail.com>
2025-02-15 16:18:07 -0500

    Update Config

--- src/main/java/vesper/pw/PaleWorldConfig.java
+++ src/main/java/vesper/pw/PaleWorldConfig.java
@@ -4,7 +4,13 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class PaleWorldConfig extends MidnightConfig {
 
-    @Comment public static Comment warn;
-    @Entry public static int palecaveweight = 4;
+    public static final String Mobs = "Mobs";
+    public static final String Gen = "Generation";
+
+    @Comment(category = Gen, centered = true) public static Comment warn;
+    @Entry(category = Gen) public static int palecaveweight = 4;
+    @Entry(category = Gen) public static int strippedOakCount = 1;
+    @Entry(category = Gen) public static float strippedOakExtraChance = 0.05F;
+    @Entry(category = Gen) public static int strippedOakExtraCount = 2;
 
 }
