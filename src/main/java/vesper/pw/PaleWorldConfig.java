d0d5f0759390daa036cb2c43ca240dd037e43875
Ves <vesmaybevesper@gmail.com>
2025-02-21 20:26:21 -0500

    Tie certain gen to config values

--- src/main/java/vesper/pw/PaleWorldConfig.java
+++ src/main/java/vesper/pw/PaleWorldConfig.java
@@ -4,11 +4,16 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class PaleWorldConfig extends MidnightConfig {
 
+    private enum horrorVals {FALSE, TRUE}
+
     public static final String Mobs = "Mobs";
     public static final String Gen = "Generation";
+    public static final String Cos = "Cosmetic";
 
     @Comment(category = Gen, centered = true) public static Comment warn;
     @Entry(category = Gen) public static int palecaveweight = 4;
+    /*@Entry(category = Cos) public horrorVals horrorModeSelect = horrorVals.FALSE;*/
+    public static Boolean horrorMode = Boolean.FALSE;
     @Entry(category = Gen) public static int strippedOakCount = 1;
     @Entry(category = Gen) public static float strippedOakExtraChance = 0.05F;
     @Entry(category = Gen) public static int strippedOakExtraCount = 2;
