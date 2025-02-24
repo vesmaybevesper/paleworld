98443ba99162c0b64f8f52a606ef251b607293a2
Ves <vesmaybevesper@gmail.com>
2025-02-24 16:43:56 -0500

    Add new trees

--- src/main/java/vesper/pw/PaleWorldConfig.java
+++ src/main/java/vesper/pw/PaleWorldConfig.java
@@ -8,16 +8,33 @@ public class PaleWorldConfig extends MidnightConfig {
 
    public enum horrorVals {FALSE, TRUE}
 
-    public static final String Mobs = "Mobs";
+    public static final String Mobs = "C_Mobs";
     public static final String Gen = "B_Generation";
     public static final String Cos = "A_Cosmetic";
 
     @Comment(category = Gen, centered = true) public static Comment warn;
     @Entry(category = Gen) public static int palecaveweight = 4;
     @Entry(category = Cos) public static horrorVals horrorModeSelect = horrorVals.FALSE;
-    @Entry(category = Gen) public static int strippedOakCount = 1;
-    @Entry(category = Gen) public static float strippedOakExtraChance = 0.05F;
-    @Entry(category = Gen) public static int strippedOakExtraCount = 2;
-
+     public static int strippedOakCount = 3;
+     public static float strippedOakExtraChance = 0.05F;
+     public static int strippedOakExtraCount = 2;
+     public static int bareOakCount = 2;
+     public static float bareOakExtraChance = 0.05F;
+     public static int bareOakExtraCount = 2;
+     public static int barestrippedOakCount = 1;
+     public static float barestrippedOakExtraChance = 0.05F;
+     public static int barestrippedOakExtraCount = 2;
+     public static int smallPaleOakChance = 4;
+     public static float smallPaleOakExtraChance = 0.05F;
+     public static int smallPaleOakExtraCount = 2;
+     public static int strippedSmallPaleOakChance = 3;
+     public static float strippedSmallPaleOakExtraChance = 0.05F;
+     public static int strippedSmallPaleOakExtraCount = 2;
+     public static int bareSmallPaleOakChance = 5;
+     public static float bareSmallPaleOakExtraChance = 0.05F;
+     public static int bareSmallPaleOakExtraCount = 2;
+     public static int bareStrippedSmallPaleOakChance = 4;
+     public static float bareStrippedSmallPaleOakExtraChance = 0.05F;
+     public static int bareStrippedSmallPaleOakExtraCount = 3;
     }
 
