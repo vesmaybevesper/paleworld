d3944d0cee41b1763f8a40813f4c01739f22c7ef
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-28 12:20:03 -0500

    fabric loader, moddev, mod publiblish and stonecutter version bumps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
 
 plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-	id("dev.kikugie.stonecutter") version "0.8-alpha.9"
+	id("dev.kikugie.stonecutter") version "0.8.1"
 }
 
 stonecutter {
