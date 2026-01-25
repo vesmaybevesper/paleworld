071020b2f56f440e7888708e6b2d72d4f7ded15b
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-01-24 21:32:19 -0500

    spare pale garden sub-biome

--- src/main/java/dev/vesper/paleworld/common/biomes/PaleWorldBiomePlacement.java
+++ src/main/java/dev/vesper/paleworld/common/biomes/PaleWorldBiomePlacement.java
@@ -9,9 +9,7 @@ public class PaleWorldBiomePlacement {
 	public static void place(){
 		BiomePlacement.replaceOverworld(Biomes.DARK_FOREST, Biomes.PALE_GARDEN, 0.15);
 		BiomePlacement.replaceOverworld(Biomes.LUSH_CAVES, PaleWorldBiomes.PALE_CAVE, 0.1);
-		BiomePlacement.replaceOverworld(Biomes.FOREST, PaleWorldBiomes.PALE_VALLEY,0.09);
-		//I want it to be a sub biome but i cant get it looking good so for now its a stand-alone biome
-		//BiomePlacement.addSubOverworld(Biomes.PALE_GARDEN, PaleWorldBiomes.PALE_VALLEY, CriterionBuilder.ratio(RatioTargets.EDGE, .3f, .5f));
+		BiomePlacement.addSubOverworld(Biomes.PALE_GARDEN, PaleWorldBiomes.PALE_VALLEY, CriterionBuilder.NEAR_BORDER);
 	}
 
 
