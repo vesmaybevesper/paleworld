e6a61d421d9af85d795b9e71caaf8f376671140d
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-06-01 17:08:36 -0400

    i missed one

--- src/main/java/dev/vesper/paleworld/common/world/gen/feature/BlankLeaves.java
+++ src/main/java/dev/vesper/paleworld/common/world/gen/feature/BlankLeaves.java
@@ -3,11 +3,11 @@ package dev.vesper.paleworld.common.world.gen.feature;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.util.valueproviders.UniformInt;
-import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
+import org.jspecify.annotations.NonNull;
 
 public class BlankLeaves extends FoliagePlacer {
 	public BlankLeaves(IntProvider intProvider, IntProvider intProvider2) {
@@ -16,20 +16,20 @@ public class BlankLeaves extends FoliagePlacer {
 	}
 
 	@Override
-	protected FoliagePlacerType<?> type() {
+	protected @NonNull FoliagePlacerType<?> type() {
 		return FoliagePlacerType.DARK_OAK_FOLIAGE_PLACER;
 	}
 
 	@Override
-	protected void createFoliage(WorldGenLevel level, FoliageSetter foliageSetter, RandomSource random, TreeConfiguration config, int treeHeight, FoliageAttachment foliageAttachment, int foliageHeight, int leafRadius, int offset) {}
+	protected void createFoliage(@NonNull WorldGenLevel level, @NonNull FoliageSetter foliageSetter, @NonNull RandomSource random, @NonNull TreeConfiguration config, int treeHeight, @NonNull FoliageAttachment foliageAttachment, int foliageHeight, int leafRadius, int offset) {}
 
 	@Override
-	public int foliageHeight(RandomSource randomSource, int i, TreeConfiguration treeConfiguration) {
+	public int foliageHeight(@NonNull RandomSource randomSource, int i, @NonNull TreeConfiguration treeConfiguration) {
 		return 0;
 	}
 
 	@Override
-	protected boolean shouldSkipLocation(RandomSource randomSource, int i, int j, int k, int l, boolean bl) {
+	protected boolean shouldSkipLocation(@NonNull RandomSource randomSource, int i, int j, int k, int l, boolean bl) {
 		return true;
 	}
 }
