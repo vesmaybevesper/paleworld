e25fab28d801b7d464b3ad9f10db078afabad47f
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-06-22 11:24:49 -0400

    add 26.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,6 +28,7 @@ stonecutter {
 			loaders.forEach { version("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
 		match("26.1.2", "fabric")
+		match("26.2", "fabric")
 
 		vcsVersion = "26.1.2-fabric"
 	}
