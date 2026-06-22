f16f55f16cb087f63ee5867bd32151ac0c778d40
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-06-22 11:57:57 -0400

    pale garden mob spawn override for 26.2

--- src/main/java/dev/vesper/paleworld/common/modify/PaleGardenMobSpawnRates.java
+++ src/main/java/dev/vesper/paleworld/common/modify/PaleGardenMobSpawnRates.java
@@ -4,6 +4,7 @@ package dev.vesper.paleworld.common.modify;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 //?}
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
@@ -14,9 +15,13 @@ import net.minecraft.world.level.biome.Biome;
 public class PaleGardenMobSpawnRates {
 	public static ResourceKey<Biome> paleGardenKey = ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath("minecraft", "pale_garden"));
 
+	public static EntityType<?> entityType(String mob){
+		return BuiltInRegistries.ENTITY_TYPE.getValue(Identifier.withDefaultNamespace(mob));
+	}
+
 	public static void override(){
-		//? fabric{
-		BiomeModifications.addSpawn(
+		//? <26.2{
+		/*BiomeModifications.addSpawn(
 				BiomeSelectors.includeByKey(paleGardenKey),
 				MobCategory.MONSTER,
 				EntityType.ZOMBIE,
@@ -78,9 +83,69 @@ public class PaleGardenMobSpawnRates {
 				1,
 				2
 		);
-		//?}
-		//? neoforge{
-		// nothing to do here cause NF spawns are data driven why TF cant the data driven stuff be the same across loaders aaaaaaa
+		*///?} >=26.2{
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("zombie"),
+				10,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("skeleton"),
+				15,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("spider"),
+				10,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("creeper"),
+				5,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("enderman"),
+				10,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("witch"),
+				5,
+				1,
+				2
+		);
+
+		BiomeModifications.addSpawn(
+				BiomeSelectors.includeByKey(paleGardenKey),
+				MobCategory.MONSTER,
+				entityType("zombie_villager"),
+				7,
+				1,
+				2
+		);
 		//?}
 	}
 
