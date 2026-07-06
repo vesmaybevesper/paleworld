63b696bcce10463e523b45d921d783a0b0ffa8cf
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-06 11:54:51 -0400

    up date surface rules for 26.2

--- src/main/java/dev/vesper/paleworld/common/biomes/surface/MaterialRules.java
+++ src/main/java/dev/vesper/paleworld/common/biomes/surface/MaterialRules.java
@@ -1,6 +1,8 @@
 package dev.vesper.paleworld.common.biomes.surface;
 
 import dev.vesper.paleworld.common.biomes.PaleWorldBiomes;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.SurfaceRules;
@@ -8,11 +10,18 @@ import net.minecraft.world.level.levelgen.SurfaceRules;
 public class MaterialRules {
 	static SurfaceRules.RuleSource MOSS = makeSateRule(Blocks.PALE_MOSS_BLOCK);
 
+	@SuppressWarnings("unchecked")
 	public static SurfaceRules.RuleSource createRules() {
-		return SurfaceRules.sequence(
+		//? <26.2{
+		/*return SurfaceRules.sequence(
 				SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(PaleWorldBiomes.PALE_CAVE), SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, MOSS))),
 				SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(PaleWorldBiomes.PALE_CAVE), SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, MOSS)))
 		);
+		*///?} >=26.2{
+		return SurfaceRules.sequence(
+				SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome((HolderGetter<Biome>) PaleWorldBiomes.PALE_CAVE), SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, MOSS))),
+				SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome((HolderGetter<Biome>) PaleWorldBiomes.PALE_CAVE), SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, MOSS))));
+		//?}
 	}
 
 	private static SurfaceRules.RuleSource makeSateRule(Block block){
